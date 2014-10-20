abafd667482d4f9feb7540df6c5c57c1ab4bf554
tjbiehle <tjbiehle@tamu.edu>
2014-10-20 13:08:59 -0500

    Adding Easing Script
    The script for the curved path for objects.

---
+++ Assets/Scripts/MoveToWaypointCurved.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var pathPoints : Transform[];
+var pathSpeeds : float[];
+
+private var nextWaypoint : Transform;
+private var nextWPind : int;
+private var currSpeed : float;
+private var currSind : int;
+
+var t : float;
+
+function Start () {
+	currSind = 0;
+	currSpeed = pathSpeeds[0];
+	nextWPind = 1;
+	nextWaypoint = pathPoints[1];
+}
+
+function Update () {	
+	// Curved follow over 20 seconds w/ constant speed over each distance
+/*	transform.position = Spline.InterpConstantSpeed( pathPoints, t, EasingType.Sine, true, true );
+	t += Time.deltaTime/20;*/
+	
+	if( Vector3.Distance( nextWaypoint.transform.position, transform.position ) < 0.1 )
+	{
+		nextWPind++;
+		if( nextWPind < pathPoints.Length )
+		{
+			nextWaypoint = pathPoints[nextWPind];
+			currSind++;
+			currSpeed = pathSpeeds[currSind] * 1.0;
+		}
+	}
+	
+	transform.position = Spline.MoveOnPath( pathPoints, transform.position, t, currSpeed, 100, EasingType.Cubic, true, true );
+}
