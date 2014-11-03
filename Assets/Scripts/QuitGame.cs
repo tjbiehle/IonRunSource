74d63275e0c6616f6770d4bba34d880418339ab1
taylorp36 <taylorp36@gmail.com>
2014-11-03 12:58:29 -0600

    adding start menu

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/StartGameFromPrevious.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameFromPrevious : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.LoadLevel ("Ion_Run_1");
+}
+}
