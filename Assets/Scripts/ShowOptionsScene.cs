10bd6021c98df7fc5f67e4068164f607d702a7ba
taylorp36 <pistol36>
2014-11-05 01:57:16 -0600

    re-created start menu (StartMenu.unity) using Kelly's GUI textures

---
+++ Assets/Scripts/ShowOptionsScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOptionsScene : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.LoadLevel ("Options");
+}
+}
---
+++ Assets/Scripts/ShowStartMenuScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowStartMenuScene : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.LoadLevel ("StartMenu");
+}
+}
