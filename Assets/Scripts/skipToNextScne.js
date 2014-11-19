309afb9ecefa6e132650e092cfc85ecdc679aa5a
You Wu <Uriana@dhcp-10-201-151-238.tamulink.tamu.edu>
2014-11-19 13:49:17 -0600

    Add open cutscene(storyline) and tutorial scenes
    backgrounds, prefabs, project file and scripts used in opening cutscene
    and tutorial scenes

---
+++ Assets/Scripts/skipToNextScne.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var nextLevel:String;
+var left:int= 780;
+var top:int = 440;
+var skipable:boolean = true;
+
+function start() {
+yield WaitForSeconds(15);
+Application.LoadLevel(nextLevel);
+}
+function OnGUI () {
+if(skipable){
+if (GUI.Button (Rect(left,top,80,20), "Skip")){
+	Application.LoadLevel(nextLevel);
+}
+}
+}
