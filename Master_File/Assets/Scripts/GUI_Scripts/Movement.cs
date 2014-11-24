ef453e716a0c255b408264cf34363f183f0a92d9
OneBeastlyLad <danny00ray@yahoo.com>
2014-11-24 14:08:31 -0600

    GUI Script
    Added a Script for GUI Elements

---
+++ Master_File/Assets/Scripts/GUI_Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	private bool moved = false;
+	public Texture2D inact;
+	public Texture2D act;
+	public GUITexture toMove;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (guiTexture.HitTest (Input.mousePosition)) 
+		{
+			guiTexture.texture = act;
+			
+		} 
+		else
+		{
+			guiTexture.texture = inact;
+		}
+		if (guiTexture.HitTest (Input.mousePosition) && Input.GetMouseButtonDown (0)) 
+		{
+			//print ("Clicked");
+			if (!moved) 
+			{
+				//guiTexture.texture = act;
+				Vector3 newY = toMove.transform.position;
+				newY.y -= 0.52f;//Time.deltaTime * 100;
+				toMove.transform.position = newY;
+				moved = !moved;
+			} 
+			else 
+			{
+				//guiTexture.texture = inact;
+				Vector3 newY = toMove.transform.position;
+				newY.y += 0.52f;//Time.deltaTime * 100;
+				toMove.transform.position = newY;
+				moved = !moved;
+			}
+		}
+	}
+
+
+}
---
+++ Master_File/Assets/Scripts/GUI_Scripts/goClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goClick : MonoBehaviour {
+	public Texture2D inact;
+	public Texture2D act;
+	private bool moved = false;
+
+	void Update()
+	{
+		if (guiTexture.HitTest (Input.mousePosition) && Input.GetMouseButtonDown (0)) 
+		{
+			//print ("Clicked");
+			if (!moved) 
+			{
+				guiTexture.texture = act;
+
+				moved = !moved;
+
+			} 
+			else 
+			{
+				guiTexture.texture = inact;
+
+				moved = !moved;
+			}
+		}
+	}
+
+}
