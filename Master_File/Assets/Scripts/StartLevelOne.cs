af7ab77f83a1d4c4ade843833cb9d092cff8c364
OneBeastlyLad <danny00ray@yahoo.com>
2014-11-24 22:41:16 -0600

    Link all scenes together

--- Master_File/Assets/Scripts/StartLevelOne.cs
+++ Master_File/Assets/Scripts/StartLevelOne.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class StartLevelOne : MonoBehaviour {
 
 	void OnMouseDown() {
-		Application.LoadLevel ("Ion_Run_1");
+		Application.LoadLevel ("openingCutScene");
 }
 }
